5e8a0ee7ac2b319e45b99b4f900ae5a148454fd6
EzeCombina <ezequielcombina1rgmail.com>
2025-05-28 11:02:28 -0300

    Comentarios y README

--- Prueba_Mapeo/cel_cam_imag_take.py
+++ Prueba_Mapeo/cel_cam_imag_take.py
@@ -4,6 +4,7 @@ import time
 import datetime
 
 # Diccionario y detector ArUco
+# Estas funciones son parte de OpenCV y permiten detectar marcadores ArUco en imágenes o videos.
 dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_250)
 parameters = cv.aruco.DetectorParameters()
 detector = cv.aruco.ArucoDetector(dictionary, parameters)
@@ -39,6 +40,7 @@ MARKER_COLOR = (0, 255, 255)
 # Cámara
 cap = cv.VideoCapture("http://192.168.1.6:8080/video")
 
+# Definimos variables globales para el printeo de posiciones
 last_print = 0
 print_interval = 3  # segundos
 
@@ -62,14 +64,18 @@ def guardar_snapshot_con_grilla(frame_con_grilla, src_pts):
     cv.imwrite(filename, snapshot)
     print(f"[INFO] Imagen guardada como {filename}")
 
-
+# Bucle principal
 while True:
+    # Leer frame de la cámara
     ret, frame = cap.read()
+    # Verificar si se leyó correctamente
     if not ret:
         break
 
+    # Convertir a escala de grises
     corners, ids, _ = detector.detectMarkers(frame)
 
+    # Dibujar los marcadores detectados
     if ids is not None:
         ids = ids.flatten()
         detected = {int(id): corner for id, corner in zip(ids, corners)}
@@ -84,6 +90,8 @@ while True:
             src_pts = np.array(src_pts, dtype=np.float32)
 
             # Homografía para insertar la grilla
+            # Con la funcion cv.getPerspectiveTransform obtenemos la matriz de transformación
+            # que mapea los puntos de la grilla a los puntos de los ArUcos detectados
             h_matrix = cv.getPerspectiveTransform(dst_pts, src_pts)
             inv_h_matrix = cv.getPerspectiveTransform(src_pts, dst_pts)
 
@@ -98,14 +106,21 @@ while True:
                 cv.line(grid_img, (0, y), (grid_width, y), line_color, 1)
 
             # Proyectar grilla
+            # La funcion cv.warpPerspective aplica la transformación de perspectiva a la imagen de la grilla
             warped = cv.warpPerspective(grid_img, h_matrix, (frame.shape[1], frame.shape[0]), flags=cv.INTER_LINEAR, borderMode=cv.BORDER_TRANSPARENT)
 
             # Separar canales y preparar máscara alpha
+            # Esta máscara se usa para mezclar la grilla con el frame original
+            # La funcion warped devuelve una imagen con 4 canales (BGRA)
+            # BGRA hace referencia a los canales azul, verde, rojo y alfa (transparencia)
             warped_bgr = warped[..., :3]
+            # alpha_mask es el canal alfa de la imagen warped
             alpha_mask = warped[..., 3]
+            # Con la funcion merge combinamos los canales de la máscara alpha en un solo canal
             alpha_mask_3 = cv.merge([alpha_mask, alpha_mask, alpha_mask])
 
             # Normalizar máscara
+            # Convertimos la máscara alpha a un rango de 0 a 1 para mezclar correctamente
             alpha = alpha_mask_3.astype(float) / 255.0
             frame_float = frame.astype(float)
             warped_float = warped_bgr.astype(float)
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # IA_VA_2025
 
-# Comprar Afiches (Blancos y Negros)
-# Imprimir tableros de ajedrez (4 o 5)
-# Imprimir ArUco
+# Comprar Afiches (Blancos y Negros) 
+# Imprimir tableros de ajedrez (4 o 5) (No necesario)
+# Imprimir ArUco (Listo)
 # Ver manera de colocar la cámara 
 # Traer cinta 
-# Probar a baja escala el ArUco y el dominio, junto con la calibración y la determinación de la pocisión 
+# Probar a baja escala el ArUco y el dominio, junto con la calibración y la determinación de la pocisión (Listo)
+# Agregar gráfico de la trayectória 
+# Hacer un programa provisorio que detecte la posición del robot, se ingrese una posición destino y el robot vaya a la misma. 
